68d11bf88314b68beb877b227ae59599686ae86f
Howard Daniels <hdaniels13tat@gmail.com>
2020-03-24 15:13:36 -0400

    added link buttons to each HTML page

---
+++ public/assets/js/add.js
@@ -0,0 +1,6 @@
+$("#homeButton").on("click", function(){
+    document.location.href="home.html";
+})
+$("#statsButton").on("click", function(){
+    document.location.href="stats.html";
+})
---
+++ public/assets/js/stats.js
@@ -0,0 +1,6 @@
+$("#homeButton").on("click", function(){
+    document.location.href="home.html";
+})
+$("#addButton").on("click", function(){
+    document.location.href="add.html";
+})
